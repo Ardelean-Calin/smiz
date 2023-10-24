ff2c5cb8bf1e99c97d0d39cd5534002c02e834dd
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2023-10-24 09:15:38 +0300

    Added smiz as a module.

--- build.zig
+++ build.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
+const smiz = @import("src/smiz.zig");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -15,11 +16,15 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("smiz", .{
+        .source_file = .{ .path = "src/smiz.zig" },
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "smiz",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/smiz.zig" },
         .target = target,
         .optimize = optimize,
     });
@@ -32,7 +37,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/smiz.zig" },
         .target = target,
         .optimize = optimize,
     });
